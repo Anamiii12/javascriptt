369e7baf4b289a0262eaa8dfbbb3ebcdc7d18e4c
Anamika Yadav <anamikaapinny12@gmail.com>
2025-11-29 15:13:00 +0000

    variables

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const accountName= 14455
+let accountEmail = "anamii@gmail.com"
+var accountpass ="1234"
+accountcity ="jaipur"
+let accountstate;
+
+//accountName =2 //not allowed 
+accountEmail ="kashish@gmail.com"
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+accountCity="bengaluru"
+console.log(accountName);
+console.table([accountName,accountEmail,accountpass,accountCity,accountstate])
+
---
+++ 02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";// treat all JS code as newer version
+// alert("hello") we are using node js not browser
+// console.log(3-4) ;console.log("anamii") avoid writing like these
+//code readability should be practised
+ console.log("anamii");
+ let name ="anamika"
+ let age=18
+ let isloggedin = false
+ let state;
+
+//datatypes
+ //number=> 2 to power 53 range
+//bigint
+//string=""
+//boolean=> true/false
+//null standalone value
+// undefined=> 
+//symbol
+
+
+//object
+
+// console.log(typeof"anamii");//undefined
+
+console.log(typeof null)//object
+
+ 
